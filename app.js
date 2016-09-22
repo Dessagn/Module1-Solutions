c7cc5a6eda47121e9654d9d4ef11126c31d3d8fa
Dessagn <temesgen.dessalegn@gmail.com>
2016-09-22 21:12:34 +0300

    style modification done

--- app.js
+++ app.js
@@ -27,20 +27,35 @@
 			if (!$scope.lunch) {
 				//If there is no lunch item in the text box, The following message will appear
 				$scope.message = 'Please, enter your Lunch list!';
+				$scope.boxColor = {
+					'border-color': 'red'
+				};
 			} else {
 				//Split the lunch list into an array with splice at ','
 				var lunchList = $scope.lunch.split(',');
-				
+
 				//Get the length of the lunch array
 				var lunchListLength = lunchList.length;
 				//If lunch is lessthan or equal to 3, 'ENJOY!'
 				if (lunchListLength <= 3) {
 					$scope.message = 'Enjoy!';
+					$scope.color = {
+						'color': 'green'
+					};
+					$scope.boxColor = {
+						'border-color': 'green'
+					};
 					//Then empty the text box
 					$scope.lunch = '';
 				} else {
 					//If the lunch list is greate than 3, 'TOO MUCH!'
 					$scope.message = 'Too much';
+					$scope.color = {
+						'color': 'red'
+					};
+					$scope.boxColor = {
+						'border-color': 'green'
+					};
 					//Then empty the text box
 					$scope.lunch = '';
 				}
